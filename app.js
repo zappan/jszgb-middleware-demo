df1043c2488008518b863ad69541b644e45a26c4
Tomislav Capan <tomislav.capan@gmail.com>
2014-03-05 19:05:04 +0100

    finalized version

--- app.js
+++ app.js
@@ -1,35 +1,14 @@
-var url     = require('url')
-  , hbs     = require('express3-handlebars')
+var hbs     = require('express3-handlebars')
   , express = require('express')
-  // , requestTypeOverride = require('./middleware/requestTypeOverride')
-  // , spaRequestFilter    = require('./middleware/spaRequestFilter')
+  , requestTypeOverride = require('./middleware/requestTypeOverride')
+  , spaRequestFilter    = require('./middleware/spaRequestFilter')
   , app = express()
   , server;
 
-function requestTypeOverride (req, res, next) {
-  // var urlExtIsJson   = url.parse(req.url).pathname.match(/\.json$/)
-  //   , qsFormatIsJson = 'json' === req.query.format;
-
-  // if (qsFormatIsJson || urlExtIsJson) {
-  //   req.headers.accept = 'application/json'
-  //   // strip '.json' extension so that it don't get parsed into req.params
-  //   if (urlExtIsJson) { req.url = req.url.replace(/\.json$/, ''); }
-  // }
-  next();
-}
-
-function spaRequestFilter (req, res, next) {
-  // var isJsonRequest = (/application\/json/).test(req.headers.accept);
-  // if (!isJsonRequest) {
-  //   return res.render('index')
-  // }
-  next();
-}
-
 // ### Configure application
 app.configure(function () {
-  // app.use(requestTypeOverride);  // allows request type override via querystring param
-  // app.use(spaRequestFilter);     // activates interceptor for serving SPA app shell HTML (*after* static middleware)
+  app.use(requestTypeOverride);  // allows request type override via querystring param
+  app.use(spaRequestFilter);     // activates interceptor for serving SPA app shell HTML (*after* static middleware)
   app.use(app.router);           // activates routing in the pipeline
 
   // # view engine setup
